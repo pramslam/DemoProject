8bbe93224c96a25a5cb500267d052ab142180e7a
pramslam <pramslam@gmail.com>
2015-03-20 15:12:24 -0700

    Animated Bust Correctly
    Bust animation complete. Activated by mouse click. Needs to be converted
    to palm trigger.

--- Assets/VRMindLabs Assets/Scripts/CPRChest.cs
+++ Assets/VRMindLabs Assets/Scripts/CPRChest.cs
@@ -7,19 +7,24 @@ public class CPRChest : MonoBehaviour {
 	//The chest dummy
 	public GameObject chestDummy;
 	private CPRChest chestComponent;
-	
+
 	//The triggering collider and its components
 	private GameObject trigger;
 	private palmClass palm;
 
 	//The boolean which activates the CPR section of the game
 	public bool readyForInteraction = false;
+	
+	//variables for animation -DLO
+	public bool isTouched = false;
+	public bool isReleased = false;
 
 	//The objects created when touched
 	private static GameObject Victory;
 
 	//Triggers when something collides with the box
 	void OnTriggerEnter(Collider other){
+
 		//If the simulation part has begun
 		if (readyForInteraction)
 		{
@@ -30,6 +35,7 @@ public class CPRChest : MonoBehaviour {
 				palm = trigger.GetComponent<palmClass>();
 				if (palm) //if it is a palm
 				{
+					//win game
 					OVRUGUI.UIObjectManager(Victory,"Victory",-55,"Victory!! Congratulations!",20,
 					                        new Color(7f / 255f, 90f / 255f, 41f / 255f, 200f / 255f));
 				}
@@ -39,7 +45,6 @@ public class CPRChest : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
---
+++ Assets/VRMindLabs Assets/Scripts/CPRanimation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CPRanimation : MonoBehaviour {
+
+	//animator
+	Animator anim;
+	
+	//variables for animation -DLO
+	public bool isTouched = false;
+	public bool isReleased = false;
+	
+	// Use this for initialization
+	void Start () {
+		//get animation controller and assign to anim -DLO
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("touched", isTouched);
+		anim.SetBool ("released", isReleased);
+		
+		if (Input.GetMouseButtonDown (0)) {
+			//play animation -DLO
+			isTouched = true;
+			isReleased = true;
+		}
+		
+		if (Input.GetMouseButtonUp (0)) {
+			//stop animation from looping -DLO
+			isTouched = false;
+		}
+	}
+}
